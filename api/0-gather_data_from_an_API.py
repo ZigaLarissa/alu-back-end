dce3e68fdae30a09960b9264576fabc1c6b43bd4
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-02-22 16:27:50 +0200

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
+"""Get the necessary libraries."""
+
+from pip._vendor import requests
+from sys import argv
+import json
+
+
 if __name__ == "__main__":
-    import requests
-    import sys
-    id = sys.argv[0]
-    response = requests.get("https://jsonplaceholder.typicode.com/todos/'$id'")
-    res = response.json()
-    print(res)
+    request_name = requests.get('https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    name_object = json.loads(request_name.text)
+    name = name_object.get('name')
+    
+    request_tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos/'.format(argv[1]))
+    tasks_object = json.loads(request_tasks.text)
+
+    tasks = {}
+    for dictionary in tasks_object:
+        tasks.update({dictionary.get('title'): dictionary.get('completed')})
+    
+    completed_tasks = []
+    for i in tasks.values():
+        if i is True:
+            completed_tasks.append(i)
+    
+    NUMBER_OF_DONE_TASKS = len(completed_tasks)
+    TOTAL_NUMBER_OF_TASKS = len(tasks)
+
+    print('Employee {} is done with tasks({}/{}):'.format(name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for k, v in tasks.items():
+        if v is True:
+            print('\t {}'.format(k))
