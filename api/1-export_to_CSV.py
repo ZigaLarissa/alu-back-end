b7a5409704ed714041ecc93f600b6248ae42d9ea
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-02-22 23:15:33 +0200

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-"""import the necessary libraries."""
+""""Module"""
 
 import csv
 import json
@@ -9,24 +8,43 @@ from sys import argv
 
 
 if __name__ == "__main__":
-
-    request_name = requests.get(
+    """
+        request user info by employee ID.
+    """
+    request_employee = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    """
+        convert json to dictionary.
+    """
+    employee = json.loads(request_employee.text)
+    """
+        extract username.
+    """
+    USERNAME = employee.get("username")
 
-    name_object = json.loads(request_name.text)
-    USERNAME = name_object.get('username')
-    USER_ID = name_object.get('id')
-
+    """
+        request user's TODO list.
+    """
     request_tasks = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    tasks_object = json.loads(request_tasks.text)
-
+    """
+        dictionary to store task status(completed) in boolean format.
+    """
     tasks = {}
-    for dictionary in tasks_object:
-        tasks.update({dictionary.get('title'): dictionary.get('completed')})
+    """
+        convert json to list of dictionaries.
+    """
+    user_tasks = json.loads(request_tasks.text)
+    """
+        loop through dictionary & get completed tasks.
+    """
+    for dictionary in user_tasks:
+        tasks.update({dictionary.get("completed"): dictionary.get("title")})
 
-    with open("{}.csv".format(argv[1]), 'w') as file:
+    """
+        export to CSV.
+    """
+    with open('{}.csv'.format(argv[1]), mode='w') as file:
         csv_writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-
         for k, v in tasks.items():
-            csv_writer.writerow([argv[1], USERNAME, v, k])
+            csv_writer.writerow([argv[1], USERNAME, k, v])
